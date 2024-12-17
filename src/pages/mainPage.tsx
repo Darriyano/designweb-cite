0f39926fdb1021790ed6f8c0b9aea496a6bc95cc
darriyano <android.stepanova@gmail.com>
2024-12-17 23:16:47 +0300

    First initial commit

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+const MainPage = () => {
+    return (<div className="wrapper">
+        <Header/>
+
+        <div className="bg_scroll">
+        </div>
+
+        <Footer/>
+    </div>)
+}
+
+export default MainPage;
